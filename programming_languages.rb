4858eeef2a1fbef04ba48d8275129cf6f39721ad
Scott Harrison <s.z.r.harrison@gmail.com>
2017-03-09 21:29:04 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,6 @@ def reformat_languages(languages)
   # your code here
   reformatted_hash = {}
   languages_names = []
-  styles = []
   languages.each do |style, languages_hash|
     languages_hash.each do |language_name, type_hash|
       languages_names << language_name
@@ -11,6 +10,7 @@ def reformat_languages(languages)
   end
 
   languages_names.each do |language_name|
+    styles = []
     languages.each do |style, languages_hash|
       if languages[style].key?( language_name )
         reformatted_hash[language_name] = languages_hash[language_name]
@@ -18,7 +18,6 @@ def reformat_languages(languages)
       end
     end
     reformatted_hash[language_name].merge!( { style: styles } )
-    styles = []
   end
   reformatted_hash
 end
